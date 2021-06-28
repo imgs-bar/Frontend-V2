e784079ac9ea9d121a4edd3fc34aa706bc769b48
Finley <t3mparmc63@gmail.com>
2021-06-28 03:12:12 +0100

    404 page

---
+++ pages/404.tsx
@@ -0,0 +1,34 @@
+import { Box, Heading, Text } from "@chakra-ui/react"
+import { Button } from "@chakra-ui/react"
+
+const ok = () => {
+
+    return (
+
+        <Box
+            textAlign="center"
+            m={400}
+        >
+
+            <Heading>
+                404
+            </Heading>
+
+            <Text fontWeight="500" as="span" color="gray.500">
+                The requested URL could not be found.
+            </Text>
+            <br></br><br />
+            <Button
+                colorScheme="telegram"
+                variant="outline"
+                onClick={() => (window.location.href = '/')}
+            >
+                Home
+            </Button>
+        </Box >
+
+
+    );
+}
+
+export default ok;
