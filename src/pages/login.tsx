b2a68a35265ab62052f77f6643b315f06857c83f
Pringles <80323633+PringlePot@users.noreply.github.com>
2021-06-28 11:38:32 +0200

    fix login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -9,6 +9,7 @@ import {
   Heading,
   Button,
 } from '@chakra-ui/react';
+import {Formik} from 'formik';
 import Navbar from '../components/Navbar';
 
 const Home = () => {
@@ -30,33 +31,45 @@ const Home = () => {
               <Heading fontWeight="bold" fontSize="30" py="2">
                 Login
               </Heading>
-              <chakra.form onSubmit={values => console.log(values)}>
-                <FormControl id="email" isRequired>
-                  <FormLabel>Email:</FormLabel>
-                  <Input
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    required
-                    variant="filled"
-                    placeholder="email@example.com"
-                    mb={5}
-                  />
-                </FormControl>
-                <FormControl id="password" mb={5} isRequired>
-                  <FormLabel>Password</FormLabel>
-                  <Input
-                    name="password"
-                    type="password"
-                    autoComplete="password"
-                    placeholder="**********"
-                    required
-                  />
-                </FormControl>
-                <Button colorScheme="telegram" type="submit">
-                  Login
-                </Button>
-              </chakra.form>
+              <Formik
+                initialValues={{email: '', password: ''}}
+                onSubmit={result => console.log(result)}
+              >
+                {({handleSubmit, isSubmitting, handleChange}) => (
+                  <form onSubmit={handleSubmit} onChange={handleChange}>
+                    <FormControl id="email" isRequired>
+                      <FormLabel>Email:</FormLabel>
+                      <Input
+                        name="email"
+                        type="email"
+                        autoComplete="email"
+                        required
+                        variant="filled"
+                        placeholder="email@example.com"
+                        mb={5}
+                      />
+                    </FormControl>
+                    <FormControl id="password" mb={5} isRequired>
+                      <FormLabel>Password</FormLabel>
+                      <Input
+                        name="password"
+                        type="password"
+                        autoComplete="password"
+                        placeholder="**********"
+                        required
+                      />
+                    </FormControl>
+                    <Button
+                      colorScheme="telegram"
+                      type="submit"
+                      isDisabled={isSubmitting}
+                      isLoading={isSubmitting}
+                    >
+                      Login
+                    </Button>
+                  </form>
+                )}
+              </Formik>
             </Box>
           </Box>
         </Box>
