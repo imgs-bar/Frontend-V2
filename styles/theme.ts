269bf05ce0dd227388183e12962d78b12c3728ce
Finley <t3mparmc63@gmail.com>
2021-06-28 02:11:51 +0100

    upload

---
+++ styles/theme.ts
@@ -0,0 +1,19 @@
+import { extendTheme } from "@chakra-ui/react"
+import { createBreakpoints } from "@chakra-ui/theme-tools"
+
+const breakpoints = createBreakpoints({
+    sm: '40em',
+    md: '52em',
+    lg: '64em',
+    xl: '80em',
+})
+
+const fontWeights = {
+    normal: 300,
+    medium: 600,
+    bold: 700
+}
+
+const theme = extendTheme({ fontWeights })
+
+export default theme
