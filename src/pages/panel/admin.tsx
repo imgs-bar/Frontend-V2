d9763494275f8387d74c2cb957375b4554e720e2
Finley <81249080+f7nl4y@users.noreply.github.com>
2021-07-19 02:47:17 +0100

    ok there fucking hell

--- src/pages/panel/admin.tsx
+++ src/pages/panel/admin.tsx
@@ -5,40 +5,24 @@ import {TableContent} from '../../components/Table/TableContent';
 import {TablePagination} from '../../components/Table/TablePagination';
 
 const Admin = () => {
-<<<<<<< HEAD
+  // const {user} = useUser();
+  // const router = useRouter();
+
+  // useEffect(() => {
+  //   if (!user) {
+  //     // add roles later since i cba to make it work now
+  //     router.push('/');
+  //   }
+  // }, []);
+
+  // return user ? (
   return (
-    <Box as="section" py="12">
-      <Box maxW={{base: 'xl', md: '7xl'}} mx="auto" px={{base: '6', md: '8'}}>
-        <Box overflowX="auto">
-          <Heading size="lg" mb="6">
-            Admin Panel
-          </Heading>
-          <TableActions />
-          <TableContent />
-          <TablePagination />
-        </Box>
-      </Box>
-    </Box>
-  );
-=======
-  const {user} = useUser();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (!user) {
-      // add roles later since i cba to make it work now
-      router.push('/');
-    }
-  }, []);
-
-  return user ? (
-    //   return (
     <>
-      <Flex>
+      {/* <Flex>
         <Sidebar display={['none', null, 'flex']} w={64} />
         <Navbar />
         <Nav />
-      </Flex>
+      </Flex> */}
 
       <Box as="section" py="95">
         <Box maxW={{base: 'xl', md: '7xl'}} mx="auto" px={{base: '6', md: '8'}}>
@@ -53,8 +37,7 @@ const Admin = () => {
         </Box>
       </Box>
     </>
-  ) : null;
-  //   );
->>>>>>> parent of 94e1222 (dsadad)
+    // ) : null;
+  );
 };
 export default Admin;
