0dd7a75cc1b05a9cbf2743fda1a4f10c0bb884ad
Finley <81249080+f7nl4y@users.noreply.github.com>
2021-07-19 02:45:01 +0100

    Revert "ok"
    This reverts commit 3e21b0bd7c63e0d6f53bea85b3eb25dc158cec44.

--- src/components/Footer/bottom.tsx
+++ src/components/Footer/bottom.tsx
@@ -1,11 +1,10 @@
-import {Text, TextProps, Center} from '@chakra-ui/react';
+import {Text, TextProps, Center} from '@chakra-ui/layout';
 import * as React from 'react';
 
-const Footer = (props: TextProps) => (
+export const Footer = (props: TextProps) => (
   <Center>
     <Text mt={10} fontSize="lg" {...props}>
       &copy; {new Date().getFullYear()} | imgs.bar
     </Text>
   </Center>
 );
-export default Footer;
--- src/components/Table/TablePagination.tsx
+++ src/components/Table/TablePagination.tsx
@@ -1,12 +1,6 @@
-import {
-  Button,
-  ButtonGroup,
-  Flex,
-  Text,
-  useColorModeValue as mode,
-} from '@chakra-ui/react';
-import * as React from 'react';
-import {data} from './_data';
+import { Button, ButtonGroup, Flex, Text, useColorModeValue as mode } from '@chakra-ui/react'
+import * as React from 'react'
+import { data } from './_data'
 
 export const TablePagination = () => {
   return (
@@ -23,5 +17,5 @@ export const TablePagination = () => {
         </Button>
       </ButtonGroup>
     </Flex>
-  );
-};
+  )
+}
