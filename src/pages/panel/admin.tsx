dbfd352093ce2e9dab602e9a47969890b7be782c
Finley <81249080+f7nl4y@users.noreply.github.com>
2021-07-19 02:45:19 +0100

    Update admin.tsx

--- src/pages/panel/admin.tsx
+++ src/pages/panel/admin.tsx
@@ -5,6 +5,7 @@ import {TableContent} from '../../components/Table/TableContent';
 import {TablePagination} from '../../components/Table/TablePagination';
 
 const Admin = () => {
+<<<<<<< HEAD
   return (
     <Box as="section" py="12">
       <Box maxW={{base: 'xl', md: '7xl'}} mx="auto" px={{base: '6', md: '8'}}>
@@ -19,5 +20,41 @@ const Admin = () => {
       </Box>
     </Box>
   );
+=======
+  const {user} = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!user) {
+      // add roles later since i cba to make it work now
+      router.push('/');
+    }
+  }, []);
+
+  return user ? (
+    //   return (
+    <>
+      <Flex>
+        <Sidebar display={['none', null, 'flex']} w={64} />
+        <Navbar />
+        <Nav />
+      </Flex>
+
+      <Box as="section" py="95">
+        <Box maxW={{base: 'xl', md: '7xl'}} mx="auto" px={{base: '6', md: '8'}}>
+          <Box overflowX="auto">
+            <Heading size="lg" mb="6">
+              Admin Panel
+            </Heading>
+            <TableActions />
+            <TableContent />
+            <TablePagination />
+          </Box>
+        </Box>
+      </Box>
+    </>
+  ) : null;
+  //   );
+>>>>>>> parent of 94e1222 (dsadad)
 };
 export default Admin;
