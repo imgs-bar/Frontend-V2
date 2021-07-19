3e21b0bd7c63e0d6f53bea85b3eb25dc158cec44
Finley <81249080+f7nl4y@users.noreply.github.com>
2021-07-19 02:35:39 +0100

    ok

--- src/components/Footer/bottom.tsx
+++ src/components/Footer/bottom.tsx
@@ -1,10 +1,11 @@
-import {Text, TextProps, Center} from '@chakra-ui/layout';
+import {Text, TextProps, Center} from '@chakra-ui/react';
 import * as React from 'react';
 
-export const Footer = (props: TextProps) => (
+const Footer = (props: TextProps) => (
   <Center>
     <Text mt={10} fontSize="lg" {...props}>
       &copy; {new Date().getFullYear()} | imgs.bar
     </Text>
   </Center>
 );
+export default Footer;
--- src/pages/panel/admin.tsx
+++ src/pages/panel/admin.tsx
@@ -1,23 +1,50 @@
-import {Box, Heading} from '@chakra-ui/react';
-import * as React from 'react';
+import {Box, Heading, Flex} from '@chakra-ui/react';
+import React, {useEffect} from 'react';
 import {TableActions} from '../../components/Table/TableActions';
 import {TableContent} from '../../components/Table/TableContent';
 import {TablePagination} from '../../components/Table/TablePagination';
 
+import Nav from '../../components/mobile-nav';
+import Navbar from '../../components/Navbar-Dash';
+import Sidebar from '../../components/Sidebar';
+import {useUser} from '../../components/user';
+import {useRouter} from 'next/router';
+
 const Admin = () => {
-  return (
-    <Box as="section" py="12">
-      <Box maxW={{base: 'xl', md: '7xl'}} mx="auto" px={{base: '6', md: '8'}}>
-        <Box overflowX="auto">
-          <Heading size="lg" mb="6">
-            Admin Panel
-          </Heading>
-          <TableActions />
-          <TableContent />
-          <TablePagination />
+  const {user} = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!user) {
+      // add roles later since i cba to make it work now
+      router.push('/');
+    }
+  }, []);
+
+  return user ? (
+    //   return (
+    <>
+      <Flex>
+        <Sidebar display={['none', null, 'flex']} w={64} />
+        <Navbar />
+        <Nav />
+      </Flex>
+
+      <Box as="section" py="95">
+        <Box maxW={{base: 'xl', md: '7xl'}} mx="auto" px={{base: '6', md: '8'}}>
+          <Box overflowX="auto">
+            <Heading size="lg" mb="6">
+              Admin Panel
+            </Heading>
+            <TableActions />
+            <TableContent />
+            <TablePagination />
+          </Box>
         </Box>
       </Box>
-    </Box>
-  );
+    </>
+  ) : null;
+  //   );
 };
+
 export default Admin;
