824bf97780886053c3e0d0f69b937cb0d5172235
Pringle <80323633+PringlePot@users.noreply.github.com>
2021-07-28 10:58:53 +0200

    ok

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -151,8 +151,8 @@ export interface User {
    * The users settings
    */
   settings: {
-    //Length of users filenames
-    urlLength: number;
+    //If the user's upload URLS should be longer.
+    longUrl: boolean;
 
     //If the user's upload URLS should consist of emojis
     emojiUrl: boolean;
